452856d81b1eef3f91a8c19d4498935a5e755b54
NMUMARL\10608780 <sudarshan.bhatt@lntinfotech.com>
2018-04-16 20:18:55 +0530

    broadcast receiver

---
+++ app/src/main/java/receivers/DownloadFinishReceiver.java
@@ -0,0 +1,21 @@
+package receivers;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+/**
+ * Created by 10608780 on 16-04-2018.
+ */
+
+public class DownloadFinishReceiver extends BroadcastReceiver {
+
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        Toast.makeText(context, intent.getStringExtra("download"), Toast.LENGTH_LONG).show();
+
+    }
+}
