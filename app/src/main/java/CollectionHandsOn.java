648f209cfa0307e251c8f1aac42cfed0731fa989
NMUMARL\10608780 <sudarshan.bhatt@lntinfotech.com>
2018-04-25 16:03:02 +0530

    broadcast receiver

---
+++ app/src/main/java/CollectionHandsOn.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+/**
+ * Created by 10608780 on 19-04-2018.
+ */
+
+public class CollectionHandsOn {
+
+    private static ArrayList<String> list = new ArrayList<String>();
+
+    public static void main(String[] args) {
+        list.add("1");
+        list.add("1");
+        list.add("1");
+        list.add("1");
+
+        System.out.print("" + list.size());
+    }
+
+
+}
